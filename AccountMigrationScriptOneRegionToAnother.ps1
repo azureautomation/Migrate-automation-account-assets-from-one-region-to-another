788fbe481dd7d3b9ebb18a511006956f44b51cc3
Ab0907 <109592962+Ab0907@users.noreply.github.com>
2022-11-03 17:42:26 +0530

    Update AccountMigrationScriptOneRegionToAnother.ps1

--- AccountMigrationScriptOneRegionToAnother.ps1
+++ AccountMigrationScriptOneRegionToAnother.ps1
@@ -755,7 +755,7 @@ Function TransferModules
 		{
 			Write-Error -Message "Unable to transfer modules to account $DestinationAutomationAccountName. Error Message: $($Error[0].Exception.Message)"
 		}
-		RemoveStorageAcc $storageAccountName $storageAccountRG $subscriptionId
+# 		RemoveStorageAcc $storageAccountName $storageAccountRG $subscriptionId
 	}
 	else
 	{
